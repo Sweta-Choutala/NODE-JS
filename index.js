82e7a8534ea284ed1e123e1f57d539a26ecb1f7c
Sweta Choutala <swetachoutala2@gmail.com>
2023-06-02 18:39:56 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = require('./app')
+
+console.log(app.z())
+
+// var a='10';
+
+const arr=[2,3,9,8,4,5,7,3,3];
+ let result =arr.filter((item)=>{
+    return item>=4
+    // return item===3
+
+console.log(item)
+})
+console.log(result)
+console.log(arr[5]);
---
+++ module.js
@@ -0,0 +1,12 @@
+// non-global => import karna padta hai
+// const fs=require('fs');
+
+// fs.writeFileSync("mihul.txt","code step by step")
+//  OR                
+// const fs = require('fs').writeFileSync
+// fs("ABC.txt","HELLO");               
+// global modules =>import nhi karna padta
+// console.log("code step by step");
+// console.log(__dirname)
+// console.log(__filename)
+
